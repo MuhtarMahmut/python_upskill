63697afc8108b0b0b21d99ee73000ac6e678da50
Muhtar <Muhtar.Mahmut0519@gmail.com>
2025-04-07 19:24:28 -0400

    Python Programming

---
+++ day01/first_python_program.py
@@ -0,0 +1,13 @@
+print('Python Programming')
+# This is a comment
+
+"""
+Multi
+Line
+Comments
+"""
+
+print("I love \"Python\"")
+print('I love "Python"')
+
+
---
+++ day01/variables.py
@@ -0,0 +1,43 @@
+"""
+Java Variables or in static typed programming languages:
+    String varNam = data
+
+
+Python Variables or in dynamicly typed progreamming languages:
+    varName = data
+
+"""
+
+name_of_student = "Elena"
+
+print(name_of_student)
+print(type(name_of_student))
+
+name_of_student = 3.5
+
+print(name_of_student)
+print(type(name_of_student))
+
+name_of_student = True
+
+print(name_of_student)
+print(type(name_of_student))
+
+s = '35'
+
+num = int(s)
+
+print(num)
+print(type(num))
+
+print("-------------------------------------")
+
+prog_language = "python"
+version = 3.13
+
+print("My programming language is {} programming language and the version is {}".format(prog_language, version))
+
+print(f"My programming language is {prog_language} programming language and the version is {version}")
+
+
+# print("My programming language is " + prog_language + 3.13)
