49be763954ea35025bc3f3527fb5c6ac9f90ceb6
Muhtar <Muhtar.Mahmut0519@gmail.com>
2025-04-07 20:26:35 -0400

    Python Programming

--- day01/variables.py
+++ day01/variables.py
@@ -41,3 +41,32 @@ print(f"My programming language is {prog_language} programming language and the 
 
 
 # print("My programming language is " + prog_language + 3.13)
+
+
+print("---------------")
+
+str1 = "python programming"
+
+print("python" in str1)
+
+print(True and True) # &&
+
+print(False or True) # ||
+
+s1 = "Java"
+s2 = "Java"
+
+print(s1 is s2) # used for checking if two reference variables are referencing to same objects (== operator of java)
+
+
+"""
+String s1 = "Java";
+String s2 = "Java";
+
+String s3 = new String("Java);
+
+s1 == s2  ===> true
+s1 == s3  ===> false
+"""
+
+
