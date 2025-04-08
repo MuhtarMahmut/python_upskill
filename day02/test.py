d2efbcdaf94e1b0f943e4fa1ff782628ebb9e8ee
Muhtar <Muhtar.Mahmut0519@gmail.com>
2025-04-08 19:28:33 -0400

    Python Programming

---
+++ day02/test.py
@@ -0,0 +1,12 @@
+
+from my_library import addition, cube
+
+print(addition(100, 200))
+print(cube(200))
+
+
+print("-------------------------------------")
+
+import my_library
+
+print(my_library.addition(400, 500))
